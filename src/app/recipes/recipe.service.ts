864abcfb9dc55b0df138875166ba31741bd79ec7
Nain <335210@cognizant.com>
2018-12-06 16:07:00 +0530

    Servies complete

--- src/app/recipes/recipe.service.ts
+++ src/app/recipes/recipe.service.ts
@@ -1,17 +1,37 @@
 import { Recipe } from "./recipe.model";
-import { EventEmitter } from "@angular/core";
+import { EventEmitter, Injectable } from "@angular/core";
+import { Ingredient } from "../shared/ingredient.model";
+import { ShoppingListService } from "../shopping-list/shopping-list.service";
 
+@Injectable()
 export class RecipeService{
 
     recipeSelected = new EventEmitter<Recipe>();
 
 
     private recipes: Recipe[] = [
-        new Recipe('A Test Recipe', 'This is simply a test', 'https://upload.wikimedia.org/wikipedia/commons/1/15/Recipe_logo.jpeg'),
-        new Recipe('Another Test Recipe', 'This is simply a test', 'https://upload.wikimedia.org/wikipedia/commons/1/15/Recipe_logo.jpeg')
+        new Recipe(
+            'Burger',
+            'This is simply a test','https://upload.wikimedia.org/wikipedia/commons/1/15/Recipe_logo.jpeg',
+            [
+                new Ingredient('Meat',1),
+                new Ingredient('French Fries', 20)
+            ]),
+        new Recipe('Biryani', 'This is simply a test', 'https://upload.wikimedia.org/wikipedia/commons/1/15/Recipe_logo.jpeg',
+        [
+            new Ingredient('Rice', 200),
+            new Ingredient('Chicken',100)
+
+        ])
       ];
+
+      constructor(private shoppingListService: ShoppingListService){}
 
       getRecipes(){
           return this.recipes.slice();
+      }
+
+      addIngredientsToShoppingList(ingredients:Ingredient[]){
+          this.shoppingListService.addIngredients(ingredients);
       }
 }
